07398e4d707b1ef25552c4678d24519b09af1ba3
Begzod10 <rimefara22@gmail.com>
2026-04-14 18:35:34 +0500

    feat: auto install requirements.txt on deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 cd /var/www/life_tracker
 git fetch origin master
 git reset --hard origin/master
+cd /var/www/life_tracker/backend
+/var/www/life_tracker/backend/venv/bin/pip install -r requirements.txt
+
 cd /var/www/life_tracker/frontend
 npm run build
 systemctl restart life_tracker.service
